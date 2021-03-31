f594c1d2e1f31b758b940740828a7ecee0dd9de9
leekwanyoung0425 <gp.89.ky@gmail.com>
2021-03-31 16:07:38 +0900

    1
    1

--- MyPortfolioCityOfHero/Assets/Script/TargetSelect.cs
+++ MyPortfolioCityOfHero/Assets/Script/TargetSelect.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TargetSelect : MonoBehaviour
 {
     public LayerMask Monster;
-    public List<GameObject> targetImage;
+    public List<Image> prefabtargetImage = new List<Image>();
+    List<Image> InsfabtargetImage =  new List<Image>();
     Canvas canvas;
     Vector2 halfsize = Vector2.zero;
     Coroutine isTargetingFollow = null;
@@ -15,6 +17,7 @@ public class TargetSelect : MonoBehaviour
         canvas = FindObjectOfType<Canvas>();
         halfsize.x = canvas.pixelRect.width / 2.0f;
         halfsize.y = canvas.pixelRect.height / 2.0f;
+        Debug.Log(halfsize);
     }
 
     void Update()
@@ -23,16 +26,26 @@ public class TargetSelect : MonoBehaviour
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+            int count = 0;
 
             if (Physics.Raycast(ray, out hit, 1000.0f, Monster))
             {
+                Debug.Log(Camera.main.WorldToScreenPoint(hit.transform.position));
+
+                foreach (Image image in prefabtargetImage)
+                {               
+                    InsfabtargetImage.Add(Instantiate(image));
+                    InsfabtargetImage[count].transform.SetParent(FindObjectOfType<Canvas>().transform);
+                    ++count;                                      
+                }
+
                 if (isTargetingFollow != null) StopCoroutine(isTargetingFollow);
-                isTargetingFollow = StartCoroutine(TargetingFollow(hit.transform));
+                isTargetingFollow = StartCoroutine(TargetingFollow(hit.transform, InsfabtargetImage));
             }
         }
     }
 
-    IEnumerator TargetingFollow(Transform target)
+    IEnumerator TargetingFollow(Transform target, List<Image> images)
     {
         Vector3 monstePosition;
         Vector3 targetUIPosition;
@@ -46,10 +59,15 @@ public class TargetSelect : MonoBehaviour
             targetUIPosition.x -= halfsize.x;
             targetUIPosition.y -= halfsize.y;
 
-            GameObject targetImageLeftUp = Instantiate(targetImage[1]);
-            targetImageLeftUp.transform.SetParent(FindObjectOfType<Canvas>().transform);
-            targetImageLeftUp.transform.localPosition = targetUIPosition;
-            targetImageLeftUp.transform.localPosition += new Vector3(100.0f, 100.0f, 0);
+
+            images[0].transform.localPosition = targetUIPosition;
+            images[0].transform.localPosition += new Vector3(-70.0f, 200.0f, 0);
+            images[1].transform.localPosition = targetUIPosition;
+            images[1].transform.localPosition += new Vector3(70.0f, 200.0f, 0);
+            images[2].transform.localPosition = targetUIPosition;
+            images[2].transform.localPosition += new Vector3(70.0f, -50.0f, 0);
+            images[3].transform.localPosition = targetUIPosition;
+            images[3].transform.localPosition += new Vector3(-70.0f, -50.0f, 0);
 
             yield return null;
         }
