d3c15369124318b0aeb7fb71732edac1b3c4280f
leekwanyoung0425 <gp.89.ky@gmail.com>
2021-04-27 20:51:46 +0900

    스킬 드레그
    스킬 드레그

--- MyPortfolioCityOfHero/Assets/Script/Skill/SkillDrag.cs
+++ MyPortfolioCityOfHero/Assets/Script/Skill/SkillDrag.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class SkillDrag : MonoBehaviour
+public class SkillDrag : MonoBehaviour,IPointerClickHandler ,IPointerDownHandler ,IDragHandler, IPointerUpHandler,IBeginDragHandler, IEndDragHandler
 {
+    Vector3 prePos;
+    public Canvas canvas;
+    List<RaycastResult> skillData;
+    GraphicRaycaster gr;
+    PointerEventData ped;
+    Transform setParent;
+    GameObject skill;
+    Image skillImage;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gr = canvas.GetComponent<GraphicRaycaster>();
+        ped = new PointerEventData(null);
+        skillData = new List<RaycastResult>();
     }
 
     // Update is called once per frame
@@ -15,4 +27,43 @@ public class SkillDrag : MonoBehaviour
     {
         
     }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+       skill = Instantiate(this.gameObject, canvas.transform);
+       skill.transform.position = eventData.position;
+       skillImage = skill.GetComponent<Image>();
+       skillImage.raycastTarget = false;
+       prePos = eventData.position;
+       eventData.selectedObject = skill;
+        
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+      Vector3 dir = (Vector3)eventData.position - prePos;
+      skill.transform.Translate(dir);
+      prePos = eventData.position;  
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+ 
+            skillImage.raycastTarget = true;
+            Destroy(skill);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+    }
+
+
 }
--- MyPortfolioCityOfHero/Assets/Script/Slot.cs
+++ MyPortfolioCityOfHero/Assets/Script/Slot.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Slot : MonoBehaviour
+public class Slot : MonoBehaviour, IDropHandler
 {
+    GameObject skillData;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +17,20 @@ public class Slot : MonoBehaviour
     {
         
     }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        skillData = eventData.selectedObject;
+        
+        if(skillData.GetComponent<SkillDataBase>() != null)
+        {
+            skillData.transform.SetParent(this.gameObject.transform);
+            skillData.transform.localPosition = Vector3.zero;
+        }
+        else
+        {
+            Destroy(eventData.selectedObject);
+        }
+    }
+
 }
