40e5e813380ddbeb5f1f2e9747b11bf351e24b6b
leekwanyoung0425 <gp.89.ky@gmail.com>
2021-04-30 20:49:43 +0900

    방향 표시
    방향 표시

---
+++ MyPortfolioCityOfHero/Assets/Script/CardinalPoints.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardinalPoints : MonoBehaviour
+{
+    public Transform playerTr;
+    public Image cardinalPoints;
+    Material myMaterial;
+    float algle;
+    float setoffSet;
+    void Start()
+    {
+        myMaterial = cardinalPoints.materialForRendering;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        algle = playerTr.eulerAngles.y;
+        setoffSet = algle / 360.0f;
+        myMaterial.SetTextureOffset("_MainTex", new Vector2(setoffSet,0));
+    }
+}
---
+++ MyPortfolioCityOfHero/Assets/Script/Item/ItemBase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class ItemBase : MonoBehaviour
+{
+    public string itemName = "";
+    public Image iconImage = null;
+}
